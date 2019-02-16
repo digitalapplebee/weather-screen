d61bed1f3618d2d6be485ca071992938d7213ab4
digitalapplebee <digitalapplebee@gmail.com>
2019-02-16 21:57:10 +0300

    Small refactoring and change background.

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,75 @@
-import React, { Component, Fragment } from 'react';
-import axios from 'axios';
-import moment from 'moment';
-import { TiWeatherCloudy } from 'react-icons/ti';
-import { IoMdTime, IoIosCalendar } from 'react-icons/io';
+import React, { Component, Fragment } from 'react'
+import axios from 'axios'
+import moment from 'moment'
+import { TiWeatherCloudy } from 'react-icons/ti'
+import { IoMdTime, IoIosCalendar } from 'react-icons/io'
 
-import 'reset-css';
+import 'reset-css'
 import st from './styles.scss'
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLoading: true,
       data: [],
       currently: {},
-    };
+    }
   }
 
   componentWillMount() {
-    this.weatherNow();
-    window.addEventListener('scroll', this.getRandomNumber);
+    this.weatherNow()
+    window.addEventListener('scroll', this.getRandomNumber)
     // window.addEventListener('click', this.getRandomNumber);
   }
 
+  componentDidMount() {
+    this.setState({isLoading: false})
+  }
+
   getRandomNumber() {
-    console.log(Math.floor(Math.random() * (256 - 1)) + 1);
+    console.log(Math.floor(Math.random() * (256 - 1)) + 1)
   }
 
   weatherNow() {
     axios.get('https://api.darksky.net/forecast/da22cd57814b7bb058ba37ba125884b4/53.9,27.56667')
     .then((response) => {
-      console.log(response);
+      console.log(response)
       this.setState({
         data: response.data,
         currently: response.data.currently,
-      });
+      })
     } 
     ).catch((error) => {
-      console.log(error);
-    });
-  };
+      console.log(error)
+    })
+  }
 
   iconRender() {
     switch(this.state.currently.icon) {
       case ("partly-cloudy-night"):
         return <TiWeatherCloudy className={st.container__icon} />
       default: 
-        console.log("Icon not found");
+        console.log("Icon not found")
     }
   }
 
   render() {
-    // isLoading
+    this.state.isLoading ? '' : ''
     
     // date
-    let date = moment.unix(this.state.currently.time).format("DD.MM.YYYY");
-    let time = moment.unix(this.state.currently.time).format("H:mm:ss");
+    let date = moment.unix(this.state.currently.time).format("DD.MM.YYYY")
+    let time = moment.unix(this.state.currently.time).format("H:mm:ss")
     // celsius
-    const temperature = ((this.state.currently.temperature - 32) * 5/9).toFixed(1);
+    const temperature = ((this.state.currently.temperature - 32) * 5/9).toFixed(1)
 
     return (
       <Fragment>
         
         <div className={st.container} onClick={this.weatherNow}>
           <div className={st.info}>
-            <p className={st.container__title}>Weather today</p>
-            <div className={st.container__location}>
+            <p className={st.title}>Weather today</p>
+            <div className={st.location}>
               Location: {this.state.data.timezone}
             </div>
             <div className={st.container__date}>
@@ -77,13 +82,13 @@ export default class App extends Component {
                 <span>{date}</span>
               </div>
             </div>
-            <div className={st.container__temperature}>
+            <div className={st.temperature}>
               {this.iconRender}
               <p>{temperature}°C</p>
             </div>
           </div>
         </div>
       </Fragment>
-    );
-  };
+    )
+  }
 }
